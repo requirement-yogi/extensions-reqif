27b2153dc925af8d5d7add4ccc76e90945ad8dd7
Adrien Ragot <aragot@play-sql.com>
2019-10-19 02:47:41 +0200

    Changed the pom.xml to apply the 'open-source' file header to all files... so, applied the header to all files too

--- src/main/java/com/playsql/extensions/reqif/xml/ReqifConfig.java
+++ src/main/java/com/playsql/extensions/reqif/xml/ReqifConfig.java
@@ -1,5 +1,25 @@
 package com.playsql.extensions.reqif.xml;
 
+/*-
+ * #%L
+ * Play SQL - ReqIF Import
+ * %%
+ * Copyright (C) 2019 Play SQL S.A.S.U.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import com.google.common.collect.Lists;
 import com.playsql.extensions.reqif.xml.ReqifXmlElements.AttributeDefinition;
 import com.playsql.extensions.reqif.xml.ReqifXmlElements.SpecObjectType;
